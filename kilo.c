53f52c4ccfc8abce478bb683da3e6287b93b575a
HarshKishorani <hkishorani7@gmail.com>
2024-12-03 02:14:40 +0530

    Move cursor  and Process Editor Keys to move cursor

--- kilo.c
+++ kilo.c
@@ -26,10 +26,24 @@
 */
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey
+{
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
 /*** data ***/
 
 struct editorConfig
 {
+    int cx, cy; // Cursor co-ordinates
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -126,16 +140,91 @@ void enableRawMode()
 }
 
 /// @brief Wait for one keypress, and return it.
-char editorReadKey()
+int editorReadKey()
 {
     int nread;
     char c;
+    
+    // Read in char c
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
     {
         if (nread == -1 && errno != EAGAIN)
             die("read");
     }
-    return c;
+
+    // If we read the escape character => special key press
+    if (c == '\x1b') 
+    {
+        // Read next 2 chars (bytes)
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if (seq[0] == '[')
+        {
+            // Handle Page-Up and Page-down keys
+            if (seq[1] >= '0' && seq[1] <= '9')
+            {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '1':
+                        return HOME_KEY;
+                    case '3':
+                        return DEL_KEY;
+                    case '4':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME_KEY;
+                    case '8':
+                        return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
+            }
+        }
+        return '\x1b';
+    }
+    else
+    {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols)
@@ -241,7 +330,7 @@ void editorDrawRows(struct abuf *ab)
             int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
             if (welcomelen > E.screencols)
                 welcomelen = E.screencols;
-            
+
             // Center the welcome message
             int padding = (E.screencols - welcomelen) / 2;
             if (padding)
@@ -300,9 +389,13 @@ void editorRefreshScreen()
 
     abAppend(&ab, "\x1b[?25l", 6); // Hide the cursor.
 
-    abAppend(&ab, "\x1b[H", 3); // Reposition it at the top-left corner so that we’re ready to draw the editor interface from top to bottom.
+    abAppend(&ab, "\x1b[H", 3); // H Command - Reposition it at the top-left corner so that we’re ready to draw the editor interface from top to bottom.
 
     editorDrawRows(&ab);
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1); // H- Command - Reposition the cursor to the desired location.
+    abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[H", 3);    // Reposition it at the top-left corner so that we’re ready to draw the editor interface from top to bottom.
     abAppend(&ab, "\x1b[?25h", 6); // Reset the cursor (Display it back).
@@ -313,11 +406,41 @@ void editorRefreshScreen()
 
 /*** input ***/
 
+void editorMoveCursor(int key)
+{
+    switch (key)
+    {
+    case ARROW_LEFT:
+        if (E.cx != 0)
+        {
+            E.cx--;
+        }
+        break;
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1)
+        {
+            E.cx++;
+        }
+        break;
+    case ARROW_UP:
+        if (E.cy != 0)
+        {
+            E.cy--;
+        }
+        break;
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1)
+        {
+            E.cy++;
+        }
+        break;
+    }
+}
+
 /// @brief Wait for a keypress, and then handle it.
 void editorProcessKeypress()
 {
-    char c = editorReadKey();
-
+    int c = editorReadKey();
     switch (c)
     {
     case CTRL_KEY('q'):
@@ -327,6 +450,29 @@ void editorProcessKeypress()
 
         exit(0);
         break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
+
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
+        break;
     }
 }
 
@@ -334,6 +480,9 @@ void editorProcessKeypress()
 
 void initEditor()
 {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
